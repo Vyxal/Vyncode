0b76c1f5c97445b6365c267d794fb38b50ee9e92
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-04-25 23:18:25 +1000

    Final bits

--- jvm/src/main/scala/Main.scala
+++ jvm/src/main/scala/Main.scala
@@ -1,8 +1,31 @@
 package vycoder
 
+import scala.io.StdIn
+import scala.collection.mutable.ListBuffer
+
 given Conversion[Int, BigDecimal] with
   def apply(i: Int): BigDecimal = BigDecimal(i)
 
 object Main:
   def main(args: Array[String]): Unit =
-    println("Hello, world!")
+    val coder = Coder()
+    val predictionObj = Predictions()
+    predictionObj.initalise()
+
+    val predicitionFunction = predictionObj.weightedPositions(
+      (x: BigDecimal) => BigDecimal("0.5").pow(x.toInt),
+      32,
+      128
+    )
+    // Read program from stdin
+
+    val program = Codepage.vyxalToInt(StdIn.readLine())
+    val encoded = coder.encode(program, predicitionFunction)
+
+    // print(f"input size: {len(int_lst)*8} bits ({len(int_lst)} bytes), output size: {len(bin_lst)} bits ({len(bin_lst)/8} bytes), ratio: {len(bin_lst)/(len(int_lst)*8)}")
+
+    println(
+      s"input size: ${program.length * 8} bits (${program.length} bytes), output size: ${encoded.length} bits (${encoded.length / 8} bytes), ratio: ${encoded.length / (program.length * 8)}"
+    )
+
+    println(Codepage.intToVyxal(encoded).mkString)
---
+++ shared/src/main/scala/codepage.scala
@@ -0,0 +1,26 @@
+package vycoder
+
+import scala.collection.mutable.ListBuffer
+object Codepage:
+  var codepage = "λƛ¬∧⟑∨⟇÷×«\n»°•ß†€"
+  codepage += "½∆ø↔¢⌐æʀʁɾɽÞƈ∞¨ "
+  codepage += "!\"#$%&'()*+,-./01"
+  codepage += "23456789:;<=>?@A"
+  codepage += "BCDEFGHIJKLMNOPQ"
+  codepage += "RSTUVWXYZ[\\]`^_abc"
+  codepage += "defghijklmnopqrs"
+  codepage += "tuvwxyz{|}~↑↓∴∵›"
+  codepage += "‹∷¤ð→←βτȧḃċḋėḟġḣ"
+  codepage += "ḭŀṁṅȯṗṙṡṫẇẋẏż√⟨⟩"
+  codepage += "‛₀₁₂₃₄₅₆₇₈¶⁋§ε¡"
+  codepage += "∑¦≈µȦḂĊḊĖḞĠḢİĿṀṄ"
+  codepage += "ȮṖṘṠṪẆẊẎŻ₌₍⁰¹²∇⌈"
+  codepage += "⌊¯±₴…□↳↲⋏⋎꘍ꜝ℅≤≥"
+  codepage += "≠⁼ƒɖ∪∩⊍£¥⇧⇩ǍǎǏǐǑ"
+  codepage += "ǒǓǔ⁽‡≬⁺↵⅛¼¾Π„‟"
+
+  def vyxalToInt(s: String): ListBuffer[Int] =
+    s.map(codepage.indexOf(_)).to(ListBuffer)
+
+  def intToVyxal(lst: ListBuffer[Int]): String =
+    lst.map(codepage(_)).mkString("")

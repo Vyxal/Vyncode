d71b2fe593516da6424d7a80dabb30eff6809a04
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-09-05 15:30:55 +0000

    Make Predictions#maxDistance an Int

--- shared/src/main/scala/Predictions.scala
+++ shared/src/main/scala/Predictions.scala
@@ -7,16 +7,16 @@ import scala.collection.mutable.ListBuffer
   */
 class Predictions(version: Int = 2):
   private val frequencies: ListBuffer[BigDecimal] = ListBuffer.fill(256)(0)
-  private val maxDistance: BigDecimal = 8
+  private val maxDistance = 8
   private val positions: ListBuffer[ListBuffer[ListBuffer[BigDecimal]]] =
-    ListBuffer.fill(maxDistance.toInt)(
+    ListBuffer.fill(maxDistance)(
       ListBuffer.fill(257)(ListBuffer.fill(256)(0))
     )
 
   for row <- Data.getData(version) do // This may change based on version
     for i <- row.indices do
       frequencies(row(i.toInt).toInt) += 1
-      for j <- 0 until maxDistance.toInt do
+      for j <- 0 until maxDistance do
         if (i - j - 1) >= 0 then
           positions(j.toInt)(row((i - j - 1).toInt).toInt)(
             row(i.toInt).toInt
@@ -29,7 +29,7 @@ class Predictions(version: Int = 2):
       beta: BigDecimal,
   )(lst: Seq[BigDecimal]): Seq[BigDecimal] =
     var out = Seq.fill(256)(BigDecimal(0))
-    for i <- (0 until maxDistance.toInt).reverse do
+    for i <- (0 until maxDistance).reverse do
       if i < lst.length then
         out = positions(i.toInt)(lst(lst.length - i.toInt - 1).toInt)
           .zip(out)
